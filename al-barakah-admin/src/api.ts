c7d78cf8b104eaa6dc566340f747903211b8ab0f
findasifrahman <findasifrahman@gmail.com>
2025-11-29 16:57:55 +0800

    unknown error

--- al-barakah-admin/src/api.ts
+++ al-barakah-admin/src/api.ts
@@ -1,7 +1,7 @@
 // src/api.ts
 
 const API_BASE =
-   import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+   import.meta.env.VITE_API_BASE_URL;// || "http://127.0.0.1:8000";
 
 export type AuthUser = {
   id: number;
